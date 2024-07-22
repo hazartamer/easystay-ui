d35b79e885e89e6aa3054682a1ee5fa68e6e51ae
hazartamer <hazartamer23@gmail.com>
2024-07-22 23:14:40 +0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AuthGuard } from './auth.guard';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),AuthGuard,AuthInterceptor]
 };
